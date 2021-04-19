30288ed14a36fc2904a8e9aa6249dd3356f715b5
pgswinter <tuannanh.nguyen.freelance@gmail.com>
2021-04-19 11:27:41 +0700

    config test

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,12 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// import "@testing-library/jest-dom/extend-expect";
+// import "jest-enzyme";
+// import Adapter from "enzyme-adapter-react-16";
+// import { configure } from "enzyme";
+// import enableHooks from "jest-react-hooks-shallow";
+
+// enableHooks(jest);
+// configure({ adapter: new Adapter() });
